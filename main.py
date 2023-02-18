6d5bdfecad9b26226f8be2ef61a31c319b126063
Stas <stasyan_2003@icloud.com>
2023-02-18 03:47:57 +0200

    create branch 'def_parse'. create def parser and 15 tests

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-def parse(query: str) -> dict:
-    return {}
+from urllib import parse
+
+def parser(url: str) -> dict:
+    params = dict(parse.parse_qsl(parse.urlsplit(url).query))
+    return params
+
 
 
 if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-
+    assert parser('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parser('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parser('http://example.com/') == {}
+    assert parser('http://example.com/?') == {}
+    assert parser('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parser('https://example.com/path/to/page?name=ferret&color=purple&place=mountains') == {'name': 'ferret', 'color': 'purple', 'place': 'mountains'}
+    assert parser('http://example.com/?name==Dima') == {'name': '=Dima'}
+    assert parser('http://example.com/?name=Oleg&age=?&') == {'name': 'Oleg', 'age': '?'}
+    assert parser('http://example.com/?name=Dima=Admin') == {'name': 'Dima=Admin'}
+    assert parser('http://example.com/?name=Dima=Admin&&===None') == {'name': 'Dima=Admin', '': '==None'}
+    assert parser('http://example.com/?&?&?&?') == {}
+    assert parser('https://example.com/path/to/page?name=ferret?color=purple') == {'name': 'ferret?color=purple'}
+    assert parser('http://example.com/&&&&') == {}
+    assert parser('http://example.com/????=????') == {'???': '????'}
+    assert parser('http://example.com/select&function?') == {}
 
 def parse_cookie(query: str) -> dict:
     return {}
